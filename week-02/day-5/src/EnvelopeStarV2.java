6eb59d16b7d0cb69bf57aee3c859de7d065135f0
Adam Szandai <adam.szandai@gmail.com>
2020-11-27 12:48:05 +0100

    Adding drawing project and modified project

--- week-02/day-5/src/EnvelopeStarV2.java
+++ week-02/day-5/src/EnvelopeStarV2.java
@@ -4,48 +4,57 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class EnvelopeStar {
+public class EnvelopeStarV2 {
     public static void mainDraw(Graphics graphics) {
         int zero = 0;
-        int mid = 160;
-        int full = 320;
+        int mid = HEIGHT / 2;
+        int full = HEIGHT;
         int step = full / 40;
 
-        for (int i = 0; zero < WIDTH / 2; i++) {
-            graphics.setColor(new Color(97, 173, 5));
-            graphics.drawLine(zero, HEIGHT / 2, WIDTH / 2, mid - step);
-            zero += step;
-            mid -= step;
-        }
+        drawEnvelopeStar(graphics, zero, mid, full, step);
 
-        zero = 0;
-        mid = 160;
+    }
 
+    private static void drawEnvelopeStar(Graphics graphics, int zero, int mid, int full, int step) {
+        getUpperLeft(graphics, zero, mid, step);
+        getBottomLeft(graphics, zero, mid, step);
+        getUpperRight(graphics, zero, mid, step);
+        getBottomRight(graphics, mid, full, step);
+    }
+
+    private static void getBottomRight(Graphics graphics, int mid, int full, int step) {
         for (int i = 0; mid < WIDTH; i++) {
             graphics.setColor(new Color(97, 173, 5));
-            graphics.drawLine(WIDTH / 2, zero, mid + step, HEIGHT / 2);
+            graphics.drawLine(WIDTH / 2, full, mid + step, HEIGHT / 2);
             mid += step;
-            zero += step;
+            full -= step;
         }
+    }
 
-        zero = 0;
-        mid = 160;
-
+    private static void getUpperRight(Graphics graphics, int zero, int mid, int step) {
         for (int i = 0; mid < HEIGHT; i++) {
             graphics.setColor(new Color(97, 173, 5));
             graphics.drawLine(zero, HEIGHT / 2, WIDTH / 2, mid + step);
             zero += step;
             mid += step;
         }
+    }
 
-        mid = 160;
-        full = 320;
-
+    private static void getBottomLeft(Graphics graphics, int zero, int mid, int step) {
         for (int i = 0; mid < WIDTH; i++) {
             graphics.setColor(new Color(97, 173, 5));
-            graphics.drawLine(WIDTH / 2, full, mid + step, HEIGHT / 2);
+            graphics.drawLine(WIDTH / 2, zero, mid + step, HEIGHT / 2);
             mid += step;
-            full -= step;
+            zero += step;
+        }
+    }
+
+    private static void getUpperLeft(Graphics graphics, int zero, int mid, int step) {
+        for (int i = 0; zero < WIDTH / 2; i++) {
+            graphics.setColor(new Color(97, 173, 5));
+            graphics.drawLine(zero, HEIGHT / 2, WIDTH / 2, mid - step);
+            zero += step;
+            mid -= step;
         }
     }
 
