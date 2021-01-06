d07c35851f0c211969b7caa7ff5c3ed0d8bcbc60
Adam Szandai <adam.szandai@gmail.com>
2021-01-06 15:22:13 +0100

    Adding Web App tasks

---
+++ week-06/day-3/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class HelloWebController {
+    AtomicLong atomicLong = new AtomicLong(1);
+
+    @RequestMapping(value = "/web/greeting")
+    public String greeting(Model model, @RequestParam String name) {
+        long counter = atomicLong.getAndIncrement();
+
+        model.addAttribute("name", name);
+        model.addAttribute("counter", counter);
+        return "greeting";
+    }
+}
