503361f21a21b8acfa1d982f9fb36c022a64961e
Adam Szandai <adam.szandai@gmail.com>
2021-01-08 14:12:31 +0100

    Adding tasks until current progress - average stock

--- week-06/day-5/webshop/src/main/java/com/project/webshop/controller/WebShopController.java
+++ week-06/day-5/webshop/src/main/java/com/project/webshop/controller/WebShopController.java
@@ -6,8 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -20,12 +20,12 @@ public class WebShopController {
                 "Running Shoes",
                 "Nike running shoes for everyday sport",
                 25000.0,
-                5));
+                8));
         shopItemsList.add(new ShopItems(
-                "Printer",
+                "Nike Printer",
                 "Some HP printer that will print pages",
                 65000.0,
-                2));
+                42));
         shopItemsList.add(new ShopItems(
                 "Coca Cola",
                 "Standard 0.5L Coca Cola",
@@ -35,7 +35,12 @@ public class WebShopController {
                 "T-Shirt",
                 "Blue with a corgi on a bike",
                 3000.0,
-                1));
+                12));
+        shopItemsList.add(new ShopItems(
+                "Flux Capacitor",
+                "Eats Uranium and can help in time travel",
+                2500000.0,
+                0));
     }
 
     @GetMapping("/webshop")
@@ -45,17 +50,51 @@ public class WebShopController {
         return "index";
     }
 
-    @GetMapping("/only-available")
-    public String onlyAvailable(Model model) {
-        model.addAttribute("itemsList", getItemAboveZeroQuantity());
-
-        return "index";
-    }
-
-
     private List<ShopItems> getItemAboveZeroQuantity() {
         return shopItemsList.stream()
                 .filter(i -> i.getQuantityOfStock() > 0)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/only-available")
+    public String onlyAvailable(Model model) {
+        model.addAttribute("itemsList", getItemAboveZeroQuantity());
+        return "index";
+    }
+
+    private List<ShopItems> getCheapestFirst() {
+        return shopItemsList.stream()
+                .sorted(Comparator.comparing(ShopItems::getPrice))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/cheapest-first")
+    public String cheapestFirst(Model model){
+        model.addAttribute("itemsList", getCheapestFirst());
+        return "index";
+    }
+
+    private List<ShopItems> getContainsNike() {
+        return shopItemsList.stream()
+                .filter(i -> i.getDescription().contains("Nike") || i.getName().contains("Nike"))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/contains-nike")
+    public String containsNike(Model model){
+        model.addAttribute("itemsList", getContainsNike());
+        return "index";
+    }
+
+    private Double getAverageStock() {
+        return shopItemsList.stream()
+                .mapToDouble(ShopItems::getQuantityOfStock)
+                .average().getAsDouble();
+    }
+
+    @GetMapping("/average-stock")
+    public String averageStock(Model model){
+        model.addAttribute("averageStock", getAverageStock());
+        return "index";
     }
 }
