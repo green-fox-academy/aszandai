115fed68fb3424fb351f82fb44f676fd3b30e917
Adam Szandai <adam.szandai@gmail.com>
2021-01-28 18:47:56 +0100

    Added more Rocket tests

--- week-09/resttest/src/test/java/com/resttest/controller/RoraControllerTest.java
+++ week-09/resttest/src/test/java/com/resttest/controller/RoraControllerTest.java
@@ -12,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.Matchers.is;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,12 +34,60 @@ class RoraControllerTest {
     public void testShipFillEmptyState() throws Exception {
         String caliber = ".25";
         int amount = 0;
-        mockMvc.perform(post("/rocket/fill?caliber=" + caliber + "&amount=" + amount))
+        boolean ready = false;
+        String shipstatus = "empty";
+        mockMvc.perform(get("/rocket/fill?caliber=" + caliber + "&amount=" + amount))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.received",is(".25")))
-                .andExpect(jsonPath("$.amount",is(0)))
-                .andExpect(jsonPath("$.ready",is(false)))
-                .andExpect(jsonPath("$.shipstatus",is("empty")))
+                .andExpect(jsonPath("$.received",is(caliber)))
+                .andExpect(jsonPath("$.amount",is(amount)))
+                .andExpect(jsonPath("$.ready",is(ready)))
+                .andExpect(jsonPath("$.shipstatus",is(shipstatus)))
                 .andDo(print());
     }
+
+    @Test
+    public void testShipFillFortyPercentState() throws Exception {
+        String caliber = ".25";
+        int amount = 5000;
+        boolean ready = false;
+        String shipstatus = "40%";
+        mockMvc.perform(get("/rocket/fill?caliber=" + caliber + "&amount=" + amount))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.received",is(caliber)))
+                .andExpect(jsonPath("$.amount",is(amount)))
+                .andExpect(jsonPath("$.ready",is(ready)))
+                .andExpect(jsonPath("$.shipstatus",is(shipstatus)))
+                .andDo(print());
+    }
+
+    @Test
+    public void testShipFillFullState() throws Exception {
+        String caliber = ".25";
+        int amount = 12500;
+        boolean ready = true;
+        String shipstatus = "full";
+        mockMvc.perform(get("/rocket/fill?caliber=" + caliber + "&amount=" + amount))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.received",is(caliber)))
+                .andExpect(jsonPath("$.amount",is(amount)))
+                .andExpect(jsonPath("$.ready",is(ready)))
+                .andExpect(jsonPath("$.shipstatus",is(shipstatus)))
+                .andDo(print());
+    }
+
+    @Test
+    public void testShipFillOverloadedState() throws Exception {
+        String caliber = ".25";
+        int amount = 15000;
+        boolean ready = false;
+        String shipstatus = "overloaded";
+        mockMvc.perform(get("/rocket/fill?caliber=" + caliber + "&amount=" + amount))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.received",is(caliber)))
+                .andExpect(jsonPath("$.amount",is(amount)))
+                .andExpect(jsonPath("$.ready",is(ready)))
+                .andExpect(jsonPath("$.shipstatus",is(shipstatus)))
+                .andDo(print());
+    }
+
 }
